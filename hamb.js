1a590623abecfe17c62c9f1a396a5415ae83cd3c
BarbaraGoncalves28 <barbaragoncalves2806@gmail.com>
2024-02-09 22:31:30 -0300

    Meu primeiro commit

---
+++ hamb.js
@@ -0,0 +1,107 @@
+b4 = window.document.getElementById('b4')
+function invadi() {
+    b4.style.background = '#ffffff'
+}
+b4 = window.getElementById('b4')
+function expulsa() {
+    b4.style.background = '#ffdb58'
+}
+
+
+
+b5 = window.document.getElementById('b5')
+function inva() {
+    b5.style.background = '#ffffff'
+}
+b5 = window.getElementById('b5')
+function expul() {
+    b5.style.background = '#ffdb58'
+}
+
+
+
+b1 = window.document.getElementById('b1')
+function entrar() {
+    b1.style.background = '#ffffff'
+}
+b1 = window.getElementById('b1')
+function sair() {
+    b1.style.background = '#ffdb58'
+}
+
+
+
+b2 = window.document.getElementById('b2')
+function entra() {
+    b2.style.background = '#ffffff'
+}
+b2 = window.getElementById('b2')
+function sai() {
+    b2.style.background = '#ffdb58'
+}
+
+
+
+b3 = window.document.getElementById('b3')
+function entr() {
+    b3.style.background = '#ffffff'
+}
+b3 = window.getElementById('b3')
+function sa() {
+    b3.style.background = '#ffdb58'
+}
+
+
+
+b6 = window.document.getElementById('b6')
+function fran() {
+    b6.style.background = '#ffffff'
+}
+b6 = window.getElementById('b6')
+function frango() {
+    b6.style.background = '#ff6961'
+}
+
+
+
+b7 = window.document.getElementById('b7')
+function ched() {
+    b7.style.background = '#ffffff'
+}
+b7 = window.getElementById('b7')
+function dar() {
+    b7.style.background = '#ff6961'
+}
+
+
+
+b8 = window.document.getElementById('b8')
+function du() {
+    b8.style.background = '#ffffff'
+}
+b8 = window.getElementById('b8')
+function plo() {
+    b8.style.background = '#ff6961'
+}
+
+
+
+b9 = window.document.getElementById('b9')
+function com() {
+    b9.style.background = '#ffffff'
+}
+b9 = window.getElementById('b9')
+function bo() {
+    b9.style.background = '#ff6961'
+}
+
+
+
+b10 = window.document.getElementById('b10')
+function tor() {
+    b10.style.background = '#ffffff'
+}
+b10 = window.getElementById('b10')
+function re() {
+    b10.style.background = '#ff6961'
+}
